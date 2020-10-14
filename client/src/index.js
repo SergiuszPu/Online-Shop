c1d00eebc8c77af35a634e19096a321cc27c2179
Sergiusz Pu <serson85@gmail.com>
2020-10-14 18:48:36 +0200

    add react-router-dom and add routing to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const Root = () => (
-  <App />
+  <Router>
+    <App />
+  </Router>
 );
 
 ReactDOM.render(<Root />, document.getElementById('root'));

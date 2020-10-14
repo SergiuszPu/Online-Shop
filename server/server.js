4d18c4778e6c5db77c1d15e02cc91031b64faeab
Sergiusz Pu <serson85@gmail.com>
2020-10-14 18:27:35 +0200

    add npm-run-all and change package.json

--- server/server.js
+++ server/server.js
@@ -8,13 +8,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/api/products', (req, res) => {
-  const data = [
-    { id: '1adfasf', title: 'Product 1', content: 'Photo 1' },
-    { id: '2evxc34', title: 'Product 2', content: 'Photo 2' },
-  ]
-  res.json(data);
+    const data = [
+        { id: '1adfasf', title: 'Product 1', content: 'Photo 1' },
+        { id: '2evxc34', title: 'Product 2', content: 'Photo 2' },
+    ]
+    res.json(data);
 });
 
-app.listen(8000, function(){
-  console.log('Server is running on port:', 8000);
+app.listen(8000, function () {
+    console.log('Server is running on port:', 8000);
 }); 

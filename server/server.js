14aed269d105160da4d3f541aa3da3c5846d171f
Sergiusz Pu <serson85@gmail.com>
2020-10-19 17:37:02 +0200

    modify file in server file

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('./config');
-const productRoutes = require('./routes/product.routes');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const productRoutes = require('./routes/product.routes');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', productRoutes);
+
+// connects our back end code with the database
+mongoose.connect(config.DB, { useNewUrlParser: true });
+let db = mongoose.connection;
+
+db.once('open', () => console.log('Connected to the database'));
+db.on('error', (err) => console.log('Error ' + err));
 
 
 app.listen(config.PORT, function () {

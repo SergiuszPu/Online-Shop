f12054fcaf92871f573cf41e44baf0d3455929da
Sergiusz Pu <serson85@gmail.com>
2020-10-19 14:05:22 +0200

    add Logo and change Navbar

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../../common/Logo/Logo';
+import MainMenu from '../../layout/MainMenu/MainMenu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import './NavBar.scss';
 
 class NavBar extends React.Component {
-
+  state = {
+    links: [
+      { path: '/', title: 'Home' },
+      { path: '/faq', title: 'FAQ' },
+      { path: '/storepolicy', title: 'Store Policy' },
+      { path: '/contact', title: 'Contact' },
+    ]
+  }
   render() {
+    const { links } = this.state;
+
     return (
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/faq'>FAQ</Link>
-        <Link to='/storepolicy'>Store Policy</Link>
-        <Link to='/contact'>Contact</Link>
-        <Link to='/cart'>Cart</Link>
+      <div className='navbar'>
+        <Logo />
+        <MainMenu links={links}>
+          <Link  to="/cart"><FontAwesomeIcon icon={faShoppingBag} size="2x"/></Link>
+        </MainMenu>
       </div>
     );
   }

31418bb82ea4c97c54b5bfa27dcefbd928d2e907
Sergiusz Pu <serson85@gmail.com>
2020-10-21 12:48:52 +0200

    add footer

--- client/src/components/layout/MainLayout/MainLayout.js
+++ client/src/components/layout/MainLayout/MainLayout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../../features/NavBar/NavBar';
 import PageContainer from '../PageContainer/PageContainer';
+import Footer from '../../features/Footer/Footer';
 
 const MainLayout = ({ children }) => (
   <div>
     <PageContainer>
       <NavBar />
       {children}
+      <Footer />
     </PageContainer>
   </div>
 

1e43bfb6fe6deb0eaa55c17f9197b64db26bc879
Sergiusz Pu <serson85@gmail.com>
2020-10-22 15:11:48 +0200

    add router transiotion

--- client/src/components/features/Footer/Footer.js
+++ client/src/components/features/Footer/Footer.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import { Container } from 'reactstrap'; 
 import FooterMenu from '../../layout/FooterMenu/FooterMenu';
 import './Footer.scss';
 
 const Footer = () => (
-  <Container>
     <footer className="footer">
         <p className="copy">&copy;2020 <a href="https://github.com/SergiuszPu"><b>SergiuszPu</b></a></p>
         <FooterMenu />
     </footer>
-  </Container>
 );
 
-export default Footer; 
+export default Footer;

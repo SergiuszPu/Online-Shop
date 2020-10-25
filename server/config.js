42695773acfe305b60997558e4789edb0855162e
Sergiusz Pu <serson85@gmail.com>
2020-10-25 17:11:49 +0100

    chenge manifest

--- server/config.js
+++ server/config.js
@@ -3,8 +3,6 @@ module.exports = {
   // DB: 'mongodb://localhost:27017/shopapp',
   // DB: 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
   PORT: process.env.PORT || 8000,
-  // DB: process.env.DB ? process.env.DB : 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
-
-  DB: (process.env.NODE_ENV === 'production') ? '/api' : 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
+  DB: process.env.DB ? process.env.DB : 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
   
 }; 

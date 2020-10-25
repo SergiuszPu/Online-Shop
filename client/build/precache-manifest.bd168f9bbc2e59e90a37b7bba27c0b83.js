51577218aba143522e15359fb63824cab93f2a23
Sergiusz Pu <serson85@gmail.com>
2020-10-25 13:48:34 +0100

    change

---
+++ client/build/precache-manifest.bd168f9bbc2e59e90a37b7bba27c0b83.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8ada697abadeca30f2611cb6929b4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6da46784ada9afc8336c",
+    "url": "/static/css/2.969319f4.chunk.css"
+  },
+  {
+    "revision": "514b78f90d6fd70af716",
+    "url": "/static/css/main.01f364e3.chunk.css"
+  },
+  {
+    "revision": "6da46784ada9afc8336c",
+    "url": "/static/js/2.fd1ce89d.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/static/js/2.fd1ce89d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514b78f90d6fd70af716",
+    "url": "/static/js/main.7cbad406.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6e14d91d55f690d1e70891bf26e4e6f2",
+    "url": "/static/media/1.6e14d91d.jpg"
+  },
+  {
+    "revision": "d78733ebdc3fb7587d4a877579914d28",
+    "url": "/static/media/2.d78733eb.jpg"
+  },
+  {
+    "revision": "a6bf4fd15db772507aa826775c95fa22",
+    "url": "/static/media/3.a6bf4fd1.jpg"
+  }
+]);

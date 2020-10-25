4467f4579978bec84111c5e901d4f86220f788e4
Sergiusz Pu <serson85@gmail.com>
2020-10-25 16:10:04 +0100

    heroku config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
+  PORT: 8000,
   // DB: 'mongodb://localhost:27017/shopapp',
-  // DB: 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
+  DB: 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
+  PORT: process.env.PORT || 8000,
   DB: process.env.DB ? process.env.DB : 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
 }; 

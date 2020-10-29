2d9e3b633c7d33c02b9ae35dbf2f9ffa32149204
Sergiusz Pu <serson85@gmail.com>
2020-10-29 11:37:49 +0100

    change

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ module.exports = {
   // PORT: 8000,
   // DB: 'mongodb://localhost:27017/shopapp',
   // DB: 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
-  PORT: process.env.PORT || 8000,
+  PORT: process.env.PORT || 3000,
   DB: process.env.DB ? process.env.DB : 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
   
 }; 

bee66f5dc846a1b169fe98620dd838700bdbd7ce
Sergiusz Pu <serson85@gmail.com>
2020-10-29 12:19:46 +0100

    change connection

---
+++ client/build/precache-manifest.c8ba72d0ca05c4ef8e433887b9e2a21c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817c2e8f21271e8f624df8a930066d28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6da46784ada9afc8336c",
+    "url": "/static/css/2.969319f4.chunk.css"
+  },
+  {
+    "revision": "bf0d4a01593be9245675",
+    "url": "/static/css/main.01f364e3.chunk.css"
+  },
+  {
+    "revision": "6da46784ada9afc8336c",
+    "url": "/static/js/2.fd1ce89d.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/static/js/2.fd1ce89d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf0d4a01593be9245675",
+    "url": "/static/js/main.c853e76a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6e14d91d55f690d1e70891bf26e4e6f2",
+    "url": "/static/media/1.6e14d91d.jpg"
+  },
+  {
+    "revision": "d78733ebdc3fb7587d4a877579914d28",
+    "url": "/static/media/2.d78733eb.jpg"
+  },
+  {
+    "revision": "a6bf4fd15db772507aa826775c95fa22",
+    "url": "/static/media/3.a6bf4fd1.jpg"
+  }
+]);
--- server/config.js
+++ server/config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // PORT: process.env.PORT || 8000,
+  // DB: process.env.DB ? process.env.PORT : 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
   PORT: process.env.PORT || 8000,
-  DB: process.env.DB ? process.env.PORT : 'mongodb+srv://serek85:serek1234@cluster0.pfssk.gcp.mongodb.net/OnlineShop?retryWrites=true&w=majority',
+  DB: process.env.DB,
 }; 
